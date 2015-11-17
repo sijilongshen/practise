79d1694f55c5786e848b74012ce76ca0c29c7970
sijilongshen <benguaer@163.com>
2015-11-18 01:09:41 +0800

    modify

--- AISM/example/esmtp.c
+++ AISM/example/esmtp.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE 500
+
 #include <stdio.h>  
 #include <stdlib.h>  
 #include <ctype.h>  
@@ -7,10 +9,27 @@
 #include <fcntl.h>  
 #include <signal.h>  
 #include <errno.h>  
-#include <stdarg.h>  
+#include <stdarg.h>
+
+#include <openssl/ssl.h>
+#include <auth-client.h>
 #include <libesmtp.h>
 
 //-hsmtp.163.com:25 -fbenguaer@163.com -sTest -m -c -t ./test-mail benguaer@163.com
+
+#define BUFLEN	8192
+
+#if !defined (__GNUC__) || __GNUC__ < 2
+# define __attribute__(x)
+#endif
+#define unused      __attribute__((unused))
+
+const char *readlinefp_cb (void **buf, int *len, void *arg);
+void  monitor_cb (const char *buf, int buflen, int writing, void *arg);
+void  print_recipient_status (smtp_recipient_t recipient, const char *mailbox, void *arg);
+int   authinteract (auth_client_request_t request, char **result, int fields, void *arg);
+int   tlsinteract (char *buf, int buflen, int rwflag, void *arg);
+void  event_cb (smtp_session_t session, int event_no, void *arg, ...);
 
 int main(int argc, char* argv[])
 {
@@ -21,14 +40,16 @@ int main(int argc, char* argv[])
 	int      nocrlf = 1;
 	int      noauth = 0;
 	int      to_cc_bcc = 0;
-	char*    file = "mail";
+	char*    file = "test-mail.eml";
 	FILE     *fp = NULL;
+	const smtp_status_t *status;
+	enum notify_flags notify = Notify_NOTSET;
 
 	struct   sigaction  sa;
 	auth_context_t  authctx;
 
-	smtp_session_t session;                                                                                                                │
-	smtp_message_t message;                                                                                                                │
+	smtp_session_t session;
+	smtp_message_t message;
 	smtp_recipient_t recipient;
 
 	auth_client_init ();
@@ -56,7 +77,7 @@ int main(int argc, char* argv[])
 
 	smtp_set_server(session, server);
 	authctx = auth_create_context();
-	auth_set_mechanism_flags (authctx, AUTH_PLUGIN_PLAIN, 0);                                                                              │
+	auth_set_mechanism_flags (authctx, AUTH_PLUGIN_PLAIN, 0);
 	auth_set_interact_cb (authctx, authinteract, NULL);
 	
 	smtp_starttls_set_password_cb (tlsinteract, NULL);
@@ -109,28 +130,248 @@ int main(int argc, char* argv[])
 		}
 
 	}
-	mtp_destroy_session (session);
+	smtp_destroy_session (session);
 	auth_destroy_context (authctx);
 	fclose (fp);
-	auth_client_exit ();                                                                                                                   │
+	auth_client_exit ();
 	exit(0);
 
 	return ret;
 
 }
 
+/* Callback to prnt the recipient status */
+void print_recipient_status (smtp_recipient_t recipient, const char *mailbox, void *arg unused)
+{
+	const smtp_status_t *status;
 
+	status = smtp_recipient_status (recipient);
+	printf ("%s: %d %s", mailbox, status->code, status->text);
+}
 
+/* Callback function to read the message from a file.  Since libESMTP
+   does not provide callbacks which translate line endings, one must
+   be provided by the application.
 
+   The message is read a line at a time and the newlines converted
+   to \r\n.  Unfortunately, RFC 822 states that bare \n and \r are
+   acceptable in messages and that individually they do not constitute a
+   line termination.  This requirement cannot be reconciled with storing
+   messages with Unix line terminations.  RFC 2822 rescues this situation
+   slightly by prohibiting lone \r and \n in messages.
 
+   The following code cannot therefore work correctly in all situations.
+   Furthermore it is very inefficient since it must search for the \n.
+ */
 
+const char *readlinefp_cb (void **buf, int *len, void *arg)
+{
+	int octets;
 
+	if (*buf == NULL)
+		*buf = malloc (BUFLEN);
 
+	if (len == NULL)
+	{
+		rewind ((FILE *) arg);
+		return NULL;
+	}
 
+	if (fgets (*buf, BUFLEN - 2, (FILE *) arg) == NULL)
+	{
+		octets = 0;
+	}
+	else
+	{
+		char *p = strchr (*buf, '\0');
 
+		if (p[-1] == '\n' && p[-2] != '\r')
+		{
+			strcpy (p - 1, "\r\n");
+			p++;
+		}
+		octets = p - (char *) *buf;
+	}
+	*len = octets;
+	return *buf;
+}
 
+void monitor_cb (const char *buf, int buflen, int writing, void *arg)
+{
+	FILE *fp = arg;
 
+	if (writing == SMTP_CB_HEADERS)
+	{
+		fputs ("H: ", fp);
+		fwrite (buf, 1, buflen, fp);
+		return;
+	}
 
+	fputs (writing ? "C: " : "S: ", fp);
+	fwrite (buf, 1, buflen, fp);
+	if (buf[buflen - 1] != '\n')
+		putc ('\n', fp);
+}
 
+/* Callback to request user/password info.  Not thread safe. */
+int authinteract (auth_client_request_t request, char **result, int fields, void *arg unused)
+{
+	char prompt[64];
+	static char resp[512];
+	char *p, *rp;
+	int i, n, tty;
+
+	rp = resp;
+	for (i = 0; i < fields; i++)
+	{
+		n = snprintf (prompt, sizeof prompt, "%s%s: ", request[i].prompt, (request[i].flags & AUTH_CLEARTEXT) ? " (not encrypted)" : "");
+		if (request[i].flags & AUTH_PASS)
+			result[i] = getpass (prompt);
+		else
+		{
+			tty = open ("/dev/tty", O_RDWR);
+			write (tty, prompt, n);
+			n = read (tty, rp, sizeof resp - (rp - resp));
+			close (tty);
+			p = rp + n;
+			while (isspace (p[-1]))
+			p--;
+			*p++ = '\0';
+			result[i] = rp;
+			rp = p;
+		}
+	}
+	return 1;
+}
+
+int tlsinteract (char *buf, int buflen, int rwflag unused, void *arg unused)
+{
+	char *pw;
+	int len;
+
+	pw = getpass ("certificate password");
+	len = strlen (pw);
+	if (len + 1 > buflen)
+		return 0;
+	
+	strcpy (buf, pw);
+	return len;
+}
+int handle_invalid_peer_certificate(long vfy_result)
+{
+	const char *k ="rare error";
+	switch(vfy_result) {
+	case X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT:
+		k="X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT"; break;
+	case X509_V_ERR_UNABLE_TO_GET_CRL:
+		k="X509_V_ERR_UNABLE_TO_GET_CRL"; break;
+	case X509_V_ERR_UNABLE_TO_DECRYPT_CERT_SIGNATURE:
+		k="X509_V_ERR_UNABLE_TO_DECRYPT_CERT_SIGNATURE"; break;
+	case X509_V_ERR_UNABLE_TO_DECRYPT_CRL_SIGNATURE:
+		k="X509_V_ERR_UNABLE_TO_DECRYPT_CRL_SIGNATURE"; break;
+	case X509_V_ERR_UNABLE_TO_DECODE_ISSUER_PUBLIC_KEY:
+		k="X509_V_ERR_UNABLE_TO_DECODE_ISSUER_PUBLIC_KEY"; break;
+	case X509_V_ERR_CERT_SIGNATURE_FAILURE:
+		k="X509_V_ERR_CERT_SIGNATURE_FAILURE"; break;
+	case X509_V_ERR_CRL_SIGNATURE_FAILURE:
+		k="X509_V_ERR_CRL_SIGNATURE_FAILURE"; break;
+	case X509_V_ERR_CERT_NOT_YET_VALID:
+		k="X509_V_ERR_CERT_NOT_YET_VALID"; break;
+	case X509_V_ERR_CERT_HAS_EXPIRED:
+		k="X509_V_ERR_CERT_HAS_EXPIRED"; break;
+	case X509_V_ERR_CRL_NOT_YET_VALID:
+		k="X509_V_ERR_CRL_NOT_YET_VALID"; break;
+	case X509_V_ERR_CRL_HAS_EXPIRED:
+		k="X509_V_ERR_CRL_HAS_EXPIRED"; break;
+	case X509_V_ERR_ERROR_IN_CERT_NOT_BEFORE_FIELD:
+		k="X509_V_ERR_ERROR_IN_CERT_NOT_BEFORE_FIELD"; break;
+	case X509_V_ERR_ERROR_IN_CERT_NOT_AFTER_FIELD:
+		k="X509_V_ERR_ERROR_IN_CERT_NOT_AFTER_FIELD"; break;
+	case X509_V_ERR_ERROR_IN_CRL_LAST_UPDATE_FIELD:
+		k="X509_V_ERR_ERROR_IN_CRL_LAST_UPDATE_FIELD"; break;
+	case X509_V_ERR_ERROR_IN_CRL_NEXT_UPDATE_FIELD:
+		k="X509_V_ERR_ERROR_IN_CRL_NEXT_UPDATE_FIELD"; break;
+	case X509_V_ERR_OUT_OF_MEM:
+		k="X509_V_ERR_OUT_OF_MEM"; break;
+	case X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT:
+		k="X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT"; break;
+	case X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN:
+		k="X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN"; break;
+	case X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY:
+		k="X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY"; break;
+	case X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE:
+		k="X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE"; break;
+	case X509_V_ERR_CERT_CHAIN_TOO_LONG:
+		k="X509_V_ERR_CERT_CHAIN_TOO_LONG"; break;
+	case X509_V_ERR_CERT_REVOKED:
+		k="X509_V_ERR_CERT_REVOKED"; break;
+	case X509_V_ERR_INVALID_CA:
+		k="X509_V_ERR_INVALID_CA"; break;
+	case X509_V_ERR_PATH_LENGTH_EXCEEDED:
+		k="X509_V_ERR_PATH_LENGTH_EXCEEDED"; break;
+	case X509_V_ERR_INVALID_PURPOSE:
+		k="X509_V_ERR_INVALID_PURPOSE"; break;
+	case X509_V_ERR_CERT_UNTRUSTED:
+		k="X509_V_ERR_CERT_UNTRUSTED"; break;
+	case X509_V_ERR_CERT_REJECTED:
+		k="X509_V_ERR_CERT_REJECTED"; break;
+	}
+	printf("SMTP_EV_INVALID_PEER_CERTIFICATE: %ld: %s\n", vfy_result, k);
+	return 1; /* Accept the problem */
+}
+
+void event_cb (smtp_session_t session, int event_no, void *arg,...)
+{
+	va_list alist;
+	int *ok;
+
+	va_start(alist, arg);
+	switch(event_no) 
+	{
+		case SMTP_EV_CONNECT: 
+		case SMTP_EV_MAILSTATUS:
+		case SMTP_EV_RCPTSTATUS:
+		case SMTP_EV_MESSAGEDATA:
+		case SMTP_EV_MESSAGESENT:
+		case SMTP_EV_DISCONNECT: break;
+		case SMTP_EV_WEAK_CIPHER: 
+			{
+				int bits;
+				bits = va_arg(alist, long); ok = va_arg(alist, int*);
+				printf("SMTP_EV_WEAK_CIPHER, bits=%d - accepted.\n", bits);
+				*ok = 1; break;
+			}
+		case SMTP_EV_STARTTLS_OK:
+			puts("SMTP_EV_STARTTLS_OK - TLS started here."); break;
+		case SMTP_EV_INVALID_PEER_CERTIFICATE: 
+		{
+			long vfy_result;
+			vfy_result = va_arg(alist, long); ok = va_arg(alist, int*);
+			*ok = handle_invalid_peer_certificate(vfy_result);
+			break;
+		}
+		case SMTP_EV_NO_PEER_CERTIFICATE: 
+		{
+			ok = va_arg(alist, int*); 
+			puts("SMTP_EV_NO_PEER_CERTIFICATE - accepted.");
+			*ok = 1; break;
+		}
+		case SMTP_EV_WRONG_PEER_CERTIFICATE: 
+		{
+			ok = va_arg(alist, int*);
+			puts("SMTP_EV_WRONG_PEER_CERTIFICATE - accepted.");
+			*ok = 1; break;
+		}
+		case SMTP_EV_NO_CLIENT_CERTIFICATE: 
+		{
+			ok = va_arg(alist, int*); 
+			puts("SMTP_EV_NO_CLIENT_CERTIFICATE - accepted.");
+			*ok = 1; break;
+		}
+		default:
+			printf("Got event: %d - ignored.\n", event_no);
+	}
+	va_end(alist);
+}
 
 
